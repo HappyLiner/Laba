4dea03bba67f7b1c68e695479f40b0a8fb9f3d8e
Semyon Kulikov <senich.99@gmail.com>
2019-12-05 03:40:42 +0300

    Added Trucks

---
+++ src/com/company/Truck.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Truck {
+    private ArrayList<Product> products;
+    private int max_weight;
+
+    Truck () {
+        this.products = new ArrayList<Product>();
+        this.max_weight = 0;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public void setMax_weight(int max_weight) {
+        this.max_weight = max_weight;
+    }
+}
